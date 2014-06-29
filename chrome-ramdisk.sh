e761f9013d87588728f9e3e52c7876202d6c95bd
Sheldon <sheldonjkennedy@gmail.com>
2014-06-29 08:51:22 -0400

    added folder as option

--- chrome-ramdisk.sh
+++ chrome-ramdisk.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# path to folder /Volumes/Ramdisk/Default
+RAMDISKFOLDER="$1"
+
+if [ $# -lt 1 ]; then
+        echo "Usage: chrome-ramdisk RAMDISKFOLDERPATH"
+        exit 1
+fi
+
 rm -rf ~/Library/Caches/Google/Chrome/Default
-ln -s /Volumes/RamDisk/Default ~/Library/Caches/Google/Chrome/Default
+ln -s RAMDISKFOLDER ~/Library/Caches/Google/Chrome/Default
 
 exit 0
